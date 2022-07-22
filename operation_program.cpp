3218281be7a1ed70eb1fe27a66c4732e09759465
Rajesh Chandra Saha <55900566+RajeshSaha2001@users.noreply.github.com>
2022-07-22 13:11:37 +0530

    Add files via upload

---
+++ operation_program.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    int a,b;
+
+    cout<<"Enter the value of a: ";
+    cin>>a;
+
+    cout <<"Enter the value of b: ";
+    cin>>b;
+
+    char op;
+
+    cout<< "Enter which operation you want to perform: ";
+
+    cin>> op;
+
+    switch ( op ) {
+
+        case '+':  cout<< (a+b) <<endl;
+                break;
+
+        case '-': cout<< (a-b) <<endl;
+                break;
+
+        case '*': cout<< (a*b) <<endl;
+                break;
+
+        case '/':cout<< (a/b) <<endl;
+                break;
+
+        case '%': cout<< (a%b) <<endl;
+                break;
+
+        default: 
+
+            cout<< "Please enter a valid operation."<<endl;
+    }
+}
